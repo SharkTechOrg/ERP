8b059d548a0526e0bf2840720866ec88698532f3
paumf23 <pauumf.89@gmail.com>
2024-11-17 14:57:42 -0300

    Subiendo con archivos completos

---
+++ tp_laravel/resources/views/empleados/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Lista de Empleados</h1>
+    <table class="table" style="max-width: 1000px; margin: 0 auto;">
+        <thead class="table-light">
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Email</th>
+                <th>Alta Contrato</th>
+                <th>Salario</th>
+                <th>Activo</th>
+                <th>Especialidad</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($empleados as $empleado)
+            <tr>
+                <td>{{ $empleado->nombre }}</td>
+                <td>{{ $empleado->apellido }}</td>
+                <td>{{ $empleado->email }}</td>
+                <td>{{ $empleado->alta_contrato }}</td>
+                <td>{{ $empleado->salario }}</td>
+                <td>{{ $empleado->activo ? 'Sí' : 'No' }}</td>
+                <td>{{ $empleado->especialidad }}</td>
+                <td>
+                    <a href="{{ route('empleados.show', $empleado->id_empleado) }}" class="btn btn-success mb-2">Ver</a>
+                    <a href="{{ route('empleados.edit', $empleado->id_empleado) }}" class="btn btn-warning">Editar</a>
+                    <form action="{{ route('empleados.destroy', $empleado->id_empleado) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+      </table>
